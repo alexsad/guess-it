764cc5dcc978fb457ba20499c30c931609d93e97
ferrugemjs <ferrugemjavascript@gmail.com>
2016-09-01 20:54:05 +0000

    better resulution support

--- config.js
+++ config.js
@@ -11,9 +11,10 @@ System.config({
     "css": "github:systemjs/plugin-css@0.1.27",
     "es6-shim": "npm:es6-shim@0.35.1",
     "event-emitter-lite": "npm:event-emitter-lite@1.0.5",
-    "ferrugemjs": "npm:ferrugemjs@0.9.0",
+    "ferrugemjs": "npm:ferrugemjs@0.9.1",
     "incremental-dom": "npm:incremental-dom@0.4.1",
     "promised-xhr": "npm:promised-xhr@1.2.0",
+    "systemjs/systemjs": "github:systemjs/systemjs@0.19.37",
     "ts-helpers": "npm:ts-helpers@1.1.1",
     "github:jspm/nodelibs-assert@0.1.0": {
       "assert": "npm:assert@1.4.1"
@@ -49,6 +50,10 @@ System.config({
     },
     "npm:es6-shim@0.35.1": {
       "process": "github:jspm/nodelibs-process@0.1.2"
+    },
+    "npm:ferrugemjs@0.9.1": {
+      "css": "github:systemjs/plugin-css@0.1.27",
+      "incremental-dom": "npm:incremental-dom@0.4.1"
     },
     "npm:inherits@2.0.1": {
       "util": "github:jspm/nodelibs-util@0.1.0"
