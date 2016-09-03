50fe3ed6955b9033fac3aa430c90eb3f446626d2
alex happy <alex.query@gmail.com>
2016-09-03 11:51:43 -0300

    Update ferrugem version

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ System.config({
     "css": "github:systemjs/plugin-css@0.1.27",
     "es6-shim": "npm:es6-shim@0.35.1",
     "event-emitter-lite": "npm:event-emitter-lite@1.0.5",
-    "ferrugemjs": "npm:ferrugemjs@0.9.1",
+    "ferrugemjs": "npm:ferrugemjs@0.9.2",
     "incremental-dom": "npm:incremental-dom@0.4.1",
     "promised-xhr": "npm:promised-xhr@1.2.0",
     "systemjs/systemjs": "github:systemjs/systemjs@0.19.37",
