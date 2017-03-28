0dad0b85094e90bcceafad6d1a81d61f83815fba
ferrugemjs <ferrugemjavascript@gmail.com>
2017-03-28 01:39:35 -0200

    test with new fjs version

--- config.js
+++ config.js
@@ -8,24 +8,21 @@ System.config({
   },
 
   map: {
-    "css": "github:systemjs/plugin-css@0.1.27",
+    "css": "github:systemjs/plugin-css@0.1.32",
     "es6-shim": "npm:es6-shim@0.35.1",
     "event-emitter-lite": "npm:event-emitter-lite@1.0.5",
-    "ferrugemjs": "npm:ferrugemjs@0.9.6",
-    "incremental-dom": "npm:incremental-dom@0.4.1",
+    "ferrugemjs": "npm:ferrugemjs@0.11.7",
+    "ferrugemjs-router": "npm:ferrugemjs-router@0.0.3",
+    "incremental-dom": "npm:incremental-dom@0.5.1",
     "js-cookie": "npm:js-cookie@2.1.3",
     "promised-xhr": "npm:promised-xhr@1.2.0",
-    "socket.io-client": "npm:socket.io-client@1.4.8",
-    "systemjs/systemjs": "github:systemjs/systemjs@0.19.37",
-    "ts-helpers": "npm:ts-helpers@1.1.1",
+    "socket.io-client": "npm:socket.io-client@1.7.3",
+    "tslib": "npm:tslib@1.5.0",
     "github:jspm/nodelibs-assert@0.1.0": {
       "assert": "npm:assert@1.4.1"
     },
     "github:jspm/nodelibs-buffer@0.1.0": {
       "buffer": "npm:buffer@3.6.0"
-    },
-    "github:jspm/nodelibs-os@0.1.0": {
-      "os-browserify": "npm:os-browserify@0.1.2"
     },
     "github:jspm/nodelibs-process@0.1.2": {
       "process": "npm:process@0.11.9"
@@ -45,9 +42,6 @@ System.config({
       "process": "github:jspm/nodelibs-process@0.1.2",
       "util": "npm:util@0.10.3"
     },
-    "npm:benchmark@1.0.0": {
-      "process": "github:jspm/nodelibs-process@0.1.2"
-    },
     "npm:better-assert@1.0.2": {
       "assert": "github:jspm/nodelibs-assert@0.1.0",
       "callsite": "npm:callsite@1.0.0",
@@ -58,51 +52,52 @@ System.config({
       "base64-js": "npm:base64-js@0.0.8",
       "child_process": "github:jspm/nodelibs-child_process@0.1.0",
       "fs": "github:jspm/nodelibs-fs@0.1.2",
-      "ieee754": "npm:ieee754@1.1.6",
+      "ieee754": "npm:ieee754@1.1.8",
       "isarray": "npm:isarray@1.0.0",
       "process": "github:jspm/nodelibs-process@0.1.2"
     },
     "npm:debug@2.2.0": {
       "ms": "npm:ms@0.7.1"
     },
-    "npm:engine.io-client@1.6.11": {
+    "npm:debug@2.3.3": {
+      "ms": "npm:ms@0.7.2"
+    },
+    "npm:engine.io-client@1.8.3": {
       "buffer": "github:jspm/nodelibs-buffer@0.1.0",
-      "component-emitter": "npm:component-emitter@1.1.2",
+      "component-emitter": "npm:component-emitter@1.2.1",
       "component-inherit": "npm:component-inherit@0.0.3",
-      "debug": "npm:debug@2.2.0",
-      "engine.io-parser": "npm:engine.io-parser@1.2.4",
+      "debug": "npm:debug@2.3.3",
+      "engine.io-parser": "npm:engine.io-parser@1.3.2",
       "has-cors": "npm:has-cors@1.1.0",
       "indexof": "npm:indexof@0.0.1",
-      "parsejson": "npm:parsejson@0.0.1",
-      "parseqs": "npm:parseqs@0.0.2",
-      "parseuri": "npm:parseuri@0.0.4",
+      "parsejson": "npm:parsejson@0.0.3",
+      "parseqs": "npm:parseqs@0.0.5",
+      "parseuri": "npm:parseuri@0.0.5",
+      "process": "github:jspm/nodelibs-process@0.1.2",
       "yeast": "npm:yeast@0.1.2"
     },
-    "npm:engine.io-parser@1.2.4": {
-      "after": "npm:after@0.8.1",
+    "npm:engine.io-parser@1.3.2": {
+      "after": "npm:after@0.8.2",
       "arraybuffer.slice": "npm:arraybuffer.slice@0.0.6",
-      "base64-arraybuffer": "npm:base64-arraybuffer@0.1.2",
+      "base64-arraybuffer": "npm:base64-arraybuffer@0.1.5",
       "blob": "npm:blob@0.0.4",
       "buffer": "github:jspm/nodelibs-buffer@0.1.0",
-      "has-binary": "npm:has-binary@0.1.6",
-      "utf8": "npm:utf8@2.1.0"
+      "has-binary": "npm:has-binary@0.1.7",
+      "wtf-8": "npm:wtf-8@1.0.0"
     },
     "npm:es6-shim@0.35.1": {
       "process": "github:jspm/nodelibs-process@0.1.2"
     },
-    "npm:ferrugemjs@0.9.6": {
-      "css": "github:systemjs/plugin-css@0.1.27",
-      "incremental-dom": "npm:incremental-dom@0.4.1"
-    },
-    "npm:has-binary@0.1.6": {
-      "buffer": "github:jspm/nodelibs-buffer@0.1.0",
-      "fs": "github:jspm/nodelibs-fs@0.1.2",
-      "isarray": "npm:isarray@0.0.1"
+    "npm:ferrugemjs-router@0.0.3": {
+      "page": "npm:page@1.7.1"
     },
     "npm:has-binary@0.1.7": {
       "buffer": "github:jspm/nodelibs-buffer@0.1.0",
       "fs": "github:jspm/nodelibs-fs@0.1.2",
       "isarray": "npm:isarray@0.0.1"
+    },
+    "npm:incremental-dom@0.5.1": {
+      "process": "github:jspm/nodelibs-process@0.1.2"
     },
     "npm:inherits@2.0.1": {
       "util": "github:jspm/nodelibs-util@0.1.0"
@@ -111,17 +106,21 @@ System.config({
       "process": "github:jspm/nodelibs-process@0.1.2",
       "url": "github:jspm/nodelibs-url@0.1.0"
     },
-    "npm:os-browserify@0.1.2": {
-      "os": "github:jspm/nodelibs-os@0.1.0"
+    "npm:page@1.7.1": {
+      "path-to-regexp": "npm:path-to-regexp@1.2.1",
+      "process": "github:jspm/nodelibs-process@0.1.2"
     },
-    "npm:parsejson@0.0.1": {
+    "npm:parsejson@0.0.3": {
       "better-assert": "npm:better-assert@1.0.2"
     },
-    "npm:parseqs@0.0.2": {
+    "npm:parseqs@0.0.5": {
       "better-assert": "npm:better-assert@1.0.2"
     },
-    "npm:parseuri@0.0.4": {
+    "npm:parseuri@0.0.5": {
       "better-assert": "npm:better-assert@1.0.2"
+    },
+    "npm:path-to-regexp@1.2.1": {
+      "isarray": "npm:isarray@0.0.1"
     },
     "npm:process@0.11.9": {
       "assert": "github:jspm/nodelibs-assert@0.1.0",
@@ -137,42 +136,33 @@ System.config({
     "npm:rsvp@3.0.9": {
       "process": "github:jspm/nodelibs-process@0.1.2"
     },
-    "npm:socket.io-client@1.4.8": {
+    "npm:socket.io-client@1.7.3": {
       "backo2": "npm:backo2@1.0.2",
       "buffer": "github:jspm/nodelibs-buffer@0.1.0",
       "component-bind": "npm:component-bind@1.0.0",
-      "component-emitter": "npm:component-emitter@1.2.0",
-      "debug": "npm:debug@2.2.0",
-      "engine.io-client": "npm:engine.io-client@1.6.11",
+      "component-emitter": "npm:component-emitter@1.2.1",
+      "debug": "npm:debug@2.3.3",
+      "engine.io-client": "npm:engine.io-client@1.8.3",
       "has-binary": "npm:has-binary@0.1.7",
       "indexof": "npm:indexof@0.0.1",
       "object-component": "npm:object-component@0.0.3",
-      "parseuri": "npm:parseuri@0.0.4",
-      "socket.io-parser": "npm:socket.io-parser@2.2.6",
+      "parseuri": "npm:parseuri@0.0.5",
+      "process": "github:jspm/nodelibs-process@0.1.2",
+      "socket.io-parser": "npm:socket.io-parser@2.3.1",
       "to-array": "npm:to-array@0.1.4"
     },
-    "npm:socket.io-parser@2.2.6": {
-      "benchmark": "npm:benchmark@1.0.0",
+    "npm:socket.io-parser@2.3.1": {
       "buffer": "github:jspm/nodelibs-buffer@0.1.0",
       "component-emitter": "npm:component-emitter@1.1.2",
       "debug": "npm:debug@2.2.0",
       "isarray": "npm:isarray@0.0.1",
       "json3": "npm:json3@3.3.2"
     },
-    "npm:ts-helpers@1.1.1": {
-      "typescript": "npm:typescript@1.8.10"
-    },
-    "npm:typescript@1.8.10": {
-      "os": "github:jspm/nodelibs-os@0.1.0"
-    },
     "npm:url@0.10.3": {
       "assert": "github:jspm/nodelibs-assert@0.1.0",
       "punycode": "npm:punycode@1.3.2",
       "querystring": "npm:querystring@0.2.0",
       "util": "github:jspm/nodelibs-util@0.1.0"
-    },
-    "npm:utf8@2.1.0": {
-      "systemjs-json": "github:systemjs/plugin-json@0.1.2"
     },
     "npm:util@0.10.3": {
       "inherits": "npm:inherits@2.0.1",
